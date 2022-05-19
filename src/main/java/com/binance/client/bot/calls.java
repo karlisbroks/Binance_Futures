46f52e197f80462ddfa3aca5ab49450e43d7bc9f
karlis <karlis.broks@gmail.com>
2022-05-19 22:59:51 +0300

    LONg and take proffit working

--- src/main/java/com/binance/client/bot/calls.java
+++ src/main/java/com/binance/client/bot/calls.java
@@ -3,18 +3,14 @@ package com.binance.client.bot;
 import com.binance.client.RequestOptions;
 import com.binance.client.SyncRequestClient;
 import com.binance.client.constant.PrivateConfig;
+import com.binance.client.model.enums.*;
 
 public class calls {
     public static void main(String[] args) {
         RequestOptions options = new RequestOptions();
         SyncRequestClient syncRequestClient = SyncRequestClient.create(PrivateConfig.API_KEY, PrivateConfig.SECRET_KEY,
                 options);
-        String out= syncRequestClient.getAccountTrades("BTCUSDT", null, null, null, null).toString();
-        out = out.replaceAll("],", "\n");
-        System.out.println(out);
-        System.out.println("Open Trades");
-        System.out.println(syncRequestClient.getOpenOrders("BTCUSDT"));
-
-
+        syncRequestClient.postOrder("ETHUSDT", OrderSide.SELL, PositionSide.LONG, OrderType.TAKE_PROFIT_MARKET, TimeInForce.GTC,
+                "0.1", null, null, null, "2200", null, NewOrderRespType.RESULT);
     }
 }
--- src/test/java/com/binance/client/examples/trade/PostOrder.java
+++ src/test/java/com/binance/client/examples/trade/PostOrder.java
@@ -6,14 +6,20 @@ import com.binance.client.SyncRequestClient;
 
 import com.binance.client.examples.constants.PrivateConfig;
 import com.binance.client.model.enums.*;
+import com.binance.client.model.trade.Order;
 
 import java.math.BigDecimal;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 public class PostOrder {
-    public static BigDecimal actualPrice;
-    public static BigDecimal closePrice;
+    public static double actualPrice;
+    public static int closePrice;
     public static int leverage=20;
+    public static int random = ThreadLocalRandom.current().nextInt(100000000, 900000000 + 1);
+    public static String orderId=Integer.toString(random);
+    private static boolean isFound;
+    public static String amount = "0.1";
 
 
     public static void main(String[] args) throws InterruptedException {
@@ -21,32 +27,45 @@ public class PostOrder {
         SyncRequestClient syncRequestClient = SyncRequestClient.create(PrivateConfig.API_KEY, PrivateConfig.SECRET_KEY,
                 options);
         SubscriptionClient client = SubscriptionClient.create();
-//        System.out.println(syncRequestClient.postOrder("BTCUSDT", OrderSide.SELL, PositionSide.BOTH, OrderType.LIMIT, TimeInForce.GTC,
-//                "1", "1", null, null, null, null));
-
-        // place dual position side order.
-        // Switch between dual or both position side, call: com.binance.client.examples.trade.ChangePositionSide
 
         //GET Latest Price
         client.subscribeMarkPriceEvent("ethusdt", ((event) -> {
-            actualPrice=event.getMarkPrice();
+            actualPrice=event.getMarkPrice().doubleValue();
             System.out.println(actualPrice);
 //            client.unsubscribeAll();
         }), null);
-        while (actualPrice==null){
+        while (actualPrice==0){
             TimeUnit.SECONDS.sleep(1);
         }
-        closePrice = actualPrice.add(actualPrice.divide(BigDecimal.valueOf(leverage)));
+        closePrice = (int) (actualPrice+actualPrice/leverage/10);
         //LONG
-        System.out.println(syncRequestClient.postOrder("ETHUSDT", OrderSide.BUY, PositionSide.LONG, OrderType.LIMIT, TimeInForce.GTC,
-                "0.1", actualPrice.toString(), null, null, null, null, NewOrderRespType.RESULT));
-//      //Set TAKE PROFFIT is order OK
+        String postOrderRs=(syncRequestClient.postOrder("ETHUSDT", OrderSide.BUY, PositionSide.LONG, OrderType.LIMIT, TimeInForce.GTC,
+                amount,
+//                "1500",
+                String.format("%.2f", actualPrice),
+                null, orderId, null, null, NewOrderRespType.RESULT)).toString();
+//      is order OK
         //Check order status
+        while (isFound!=true){
+            String orderRs=syncRequestClient.getOrder("ETHUSDT",
+                    (long) random,
+//                    858143724L,
+                    null).toString();
+            isFound = orderRs.contains("FILLED");
+          System.out.println("NEW");
+            TimeUnit.SECONDS.sleep(1);
+        }
+//
+//      //Set TAKE PROFFIT
+        String takeProffitOrder = (syncRequestClient.postOrder("ETHUSDT",OrderSide.SELL, PositionSide.LONG, OrderType.TAKE_PROFIT_MARKET, TimeInForce.GTC,
+                amount, null, null, null, Integer.toString(closePrice), null, NewOrderRespType.RESULT)).toString();
 
-//        boolean isFound = text.contains("Java8");
-
-        System.out.println(syncRequestClient.postOrder("ETHUSDT",OrderSide.SELL, PositionSide.LONG, OrderType.TAKE_PROFIT_MARKET, TimeInForce.GTC,
-                "0.1", null, null, null, closePrice.toString(), null, NewOrderRespType.RESULT));
-
+        //Report
+        System.out.println("Position created!");
+        System.out.println("Actual price: "+actualPrice);
+        System.out.println("Order filled: "+ isFound);
+        System.out.println("Order ID: "+orderId);
+        System.out.println("Take Profit set!");
+        System.out.println("Close price: "+closePrice);
     }
 }
